e7493b476b3b34f481cf2cbe41aee9b450978946
corporatesim <das_debasish@hotmail.com>
2018-08-31 16:32:53 +0530

    * checked

--- autologin.php
+++ autologin.php
@@ -6,27 +6,26 @@ if(isset($cookie_name)){
 		parse_str($_COOKIE[$cookie_name]);
 
 		$fields = array();
-		$where = array("username='".$usr."'", "password='".$hash."'");
-		$sql = "SELECT
-					u.*
-				FROM
-					".siteusers." as u
-					INNER JOIN
-					".logindetails." as l
-					ON
-					u.id = l.uid
-					WHERE
-						l.username='".$functionsObj->EscapeString($usr)."'
-						OR
-						u.email='".$functionsObj->EscapeString($usr)."'
-						AND
-						l.password='".$hash."'
-				";
+		$where  = array("username='".$usr."'", "password='".$hash."'");
+		$sql    = "SELECT
+		u.*
+		FROM
+		".siteusers." as u
+		INNER JOIN
+		".logindetails." as l
+		ON
+		u.id = l.uid
+		WHERE
+		l.username='".$functionsObj->EscapeString($usr)."'
+		OR
+		u.email='".$functionsObj->EscapeString($usr)."'
+		AND
+		l.password='".$hash."'
+		";
 		$result = $functionsObj->ExecuteQuery($sql);
 		if($result->num_rows > 0){
-			$result = $functionsObj->FetchObject($result);
+			$result                 = $functionsObj->FetchObject($result);
 			$_SESSION['pizza_user'] = $result->id;
 		}
 	}
 }
-?>
