163911098e649ef3e7f8080f7c4832cad1f94268
corporatesim <das_debasish@hotmail.com>
2018-08-31 17:13:53 +0530

    * committed via sourcetree

--- chart.php
+++ chart.php
@@ -28,61 +28,61 @@ if($countRow > 0){
 	while($ResultchartDetails= $chartDetails->fetch_object()){
 
 
-			$sqllink = "SELECT * FROM `GAME_LINKAGE` WHERE `Link_GameID`=".$gameid." AND Link_ScenarioID= ". $ResultchartDetails->Chart_ScenarioID;
-			$LinkId =  $functionsObj->ExecuteQuery($sqllink);
-			$ResultLinkId = $functionsObj->FetchObject($LinkId);
-			
-			if(!empty($ResultLinkId))
-			{
-				$sqlexist="SELECT * FROM `GAME_INPUT` i WHERE i.input_user=".$userid." and i.input_sublinkid in 
-				(SELECT s.SubLink_ID FROM GAME_LINKAGE_SUB s WHERE s.SubLink_LinkID=".$ResultLinkId->Link_ID.") group by i.input_sublinkid";
-				$object = $functionsObj->ExecuteQuery($sqlexist);
+		$sqllink = "SELECT * FROM `GAME_LINKAGE` WHERE `Link_GameID`=".$gameid." AND Link_ScenarioID= ". $ResultchartDetails->Chart_ScenarioID;
+		$LinkId =  $functionsObj->ExecuteQuery($sqllink);
+		$ResultLinkId = $functionsObj->FetchObject($LinkId);
+		
+		if(!empty($ResultLinkId))
+		{
+			$sqlexist="SELECT * FROM `GAME_INPUT` i WHERE i.input_user=".$userid." and i.input_sublinkid in 
+			(SELECT s.SubLink_ID FROM GAME_LINKAGE_SUB s WHERE s.SubLink_LinkID=".$ResultLinkId->Link_ID.") group by i.input_sublinkid";
+			$object = $functionsObj->ExecuteQuery($sqlexist);
 
 
 				//chart_component and subcomponent
 
-				$comp = $ResultchartDetails->Chart_Components;
-				$subcomp = $ResultchartDetails->Chart_Subcomponents;
-				$chartname [$i]= $ResultchartDetails->Chart_Name;
-				$charttype [$i]= $ResultchartDetails->Chart_Type;
+			$comp = $ResultchartDetails->Chart_Components;
+			$subcomp = $ResultchartDetails->Chart_Subcomponents;
+			$chartname [$i]= $ResultchartDetails->Chart_Name;
+			$charttype [$i]= $ResultchartDetails->Chart_Type;
 
-				$arrayComp = explode(',',$comp);
-				$arraysubcomp = explode(',',$subcomp);
+			$arrayComp = explode(',',$comp);
+			$arraysubcomp = explode(',',$subcomp);
 				//print_r($arrayComp);
 
 				//echo $object->num_rows;
-				if($object->num_rows > 0){
+			if($object->num_rows > 0){
+				
+				while($row= $object->fetch_object()){
 					
-					while($row= $object->fetch_object()){
+					$compdata = $row->input_key;
+					$allcompID = explode('_',$compdata);
+					$compID = $allcompID[2];
+					
+					if (in_array($compID, $arrayComp) && $allcompID[1]=='comp')
+					{
+						$sqlcompName = "SELECT Comp_Name FROM GAME_COMPONENT WHERE Comp_ID = ".$compID;
+						$compDetails = $functionsObj->ExecuteQuery($sqlcompName);
+						$compName= $functionsObj->FetchObject($compDetails);
 						
-						$compdata = $row->input_key;
-						$allcompID = explode('_',$compdata);
-						$compID = $allcompID[2];
-						
-						if (in_array($compID, $arrayComp) && $allcompID[1]=='comp')
-						{
-							$sqlcompName = "SELECT Comp_Name FROM GAME_COMPONENT WHERE Comp_ID = ".$compID;
-							$compDetails = $functionsObj->ExecuteQuery($sqlcompName);
-							$compName= $functionsObj->FetchObject($compDetails);
-							
-							$data[$i][$compName->Comp_Name]  = $row->input_current;
-						}
-						
-						else if (in_array($compID, $arraysubcomp) && $allcompID[1]=='subc')
-						{
-							$sqlsubcompName = "SELECT SubComp_Name FROM GAME_SUBCOMPONENT WHERE SubComp_ID = ".$compID;
-							$subcompDetails = $functionsObj->ExecuteQuery($sqlsubcompName);
-							$subcompName= $functionsObj->FetchObject($subcompDetails);
-							
-							$data[$i][$subcompName->SubComp_Name]  .= $row->input_current;
-						}
-						
-						
+						$data[$i][$compName->Comp_Name]  = $row->input_current;
 					}
+					
+					else if (in_array($compID, $arraysubcomp) && $allcompID[1]=='subc')
+					{
+						$sqlsubcompName = "SELECT SubComp_Name FROM GAME_SUBCOMPONENT WHERE SubComp_ID = ".$compID;
+						$subcompDetails = $functionsObj->ExecuteQuery($sqlsubcompName);
+						$subcompName= $functionsObj->FetchObject($subcompDetails);
+						
+						$data[$i][$subcompName->SubComp_Name]  .= $row->input_current;
+					}
+					
+					
 				}
 			}
+		}
 		$i++;
-			
+		
 	}
 	
 }
@@ -90,14 +90,13 @@ if($countRow > 0){
 //echo $i++;
 
 //print_r($data);
-						
+
 //$url = site_root."chart.php?Scenario=".$result->Link_ScenarioID;
 
 include_once doc_root.'views/chart.php';
 
-					
+
 
 
 ?>
 
-				
