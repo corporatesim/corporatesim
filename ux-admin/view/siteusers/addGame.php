2fb21794e7e7494f94d9a197158b85fb4674cfbb
corporatesim <das_debasish@hotmail.com>
2018-10-29 11:30:31 +0530

    * select all feature added, while assigning game to users.

--- ux-admin/view/siteusers/addGame.php
+++ ux-admin/view/siteusers/addGame.php
@@ -16,87 +16,92 @@ span.alert-danger {
 		<ul class="breadcrumb">
 			<li class="completed"><a
 				href="<?php echo site_root."ux-admin/Dashboard"; ?>">Home</a></li>
-			<li class="active"><a href="javascript:void(0);">Manage SiteUsers</a></li>
-			<li class="active"><a
-				href="<?php echo site_root."ux-admin/siteusers"; ?>">
-					Games</a></li>
-			<li class="active"><?php echo $header; ?></li>
-		</ul>
+				<li class="active"><a href="javascript:void(0);">Manage SiteUsers</a></li>
+				<li class="active"><a
+					href="<?php echo site_root."ux-admin/siteusers"; ?>">
+				Games</a></li>
+				<li class="active"><?php echo $header; ?></li>
+			</ul>
+		</div>
 	</div>
-</div>
 
-<!-- DISPLAY ERROR MESSAGE -->
-<?php if(isset($msg)){ ?>
-<div class="form-group <?php echo $type[1]; ?>">
-	<div align="center" id="<?php echo $type[0]; ?>">
-		<label class="control-label" for="<?php echo $type[0]; ?>">
-				<?php echo $msg; ?>
-			</label>
-	</div>
-</div>
-<?php } ?>
-<!-- DISPLAY ERROR MESSAGE END -->
+	<!-- DISPLAY ERROR MESSAGE -->
+	<?php if(isset($msg)){ ?>
+		<div class="form-group <?php echo $type[1]; ?>">
+			<div align="center" id="<?php echo $type[0]; ?>">
+				<label class="control-label" for="<?php echo $type[0]; ?>">
+					<?php echo $msg; ?>
+				</label>
+			</div>
+		</div>
+	<?php } ?>
+	<!-- DISPLAY ERROR MESSAGE END -->
 
-<div class="col-sm-10">
-	<form method="POST" action="" id="siteuser_frm" name="siteuser_frm">
-		<label for="name"><span class="alert-danger">*</span>Select Games</label>
-		<input type="hidden" name="id"
-			value="<?php if(isset($_GET['edit'])){ echo $userdetails->User_id; } ?>">
-		<div class="row">
-		<div class="panel panel-default">
-		<div class="panel-body">
-		<?php
+	<div class="col-sm-10">
+		<form method="POST" action="" id="siteuser_frm" name="siteuser_frm">
+			<div class="row">
+				<div class="col-md-6">
+					<label for="name"><span class="alert-danger">*</span>Select Games</label>
+					<input type="hidden" name="id" value="<?php if(isset($_GET['edit'])){ echo $userdetails->User_id; } ?>">
+				</div>
+				<div class="col-md-6">
+					<input type="checkbox" name="select_all" id="select_all">
+					<label for="name">Selecte All</label>
+				</div>
+			</div>
+			<div class="row">
+				<div class="panel panel-default">
+					<div class="panel-body">
+						<?php
 		//$sql = 'select name from STUDENT';
 		//$result = mysqli_query($connection_object,$sql);
 
-		$id = base64_decode($_GET['edit']);		
-		$result = $functionsObj->SelectData(array(), 'GAME_GAME', array('Game_Delete=0'), '', '', '', '', 0);
+						$id     = base64_decode($_GET['edit']);		
+						$result = $functionsObj->SelectData(array(), 'GAME_GAME', array('Game_Delete=0'), '', '', '', '', 0);
+						while($row = mysqli_fetch_array($result)) {
+							$object = $functionsObj->SelectData(array(), 'GAME_USERGAMES', array('UG_UserID='.$id, 'UG_GameID='.$row['Game_ID']), '', '', '', '', 0);
 
-		while($row = mysqli_fetch_array($result)) {
-			
-			$object = $functionsObj->SelectData(array(), 'GAME_USERGAMES', array('UG_UserID='.$id, 'UG_GameID='.$row['Game_ID']), '', '', '', '', 0);
-
-			echo "<div class='col-sm-4'><div class='checkbox'";
-			if($object->num_rows > 0)
-			{
-				echo "style='background:#ccc;'><input type='checkbox' checked='checked' name='usergame[]' value='{$row['Game_ID']}'>" . $row['Game_Name'];
-			}
-			else{
-				echo "><input type='checkbox' name='usergame[]' value='{$row['Game_ID']}'>" . $row['Game_Name'];
-			}
-			echo "</div></div>";
-		}
-	 ?>
-		</div>
-		</div>
-		</div>
-		<div class="row">
-			<div class="col-sm-12">
-				<div class="form-group text-center">
-				<?php if(isset($_GET['edit']) && !empty($_GET['edit'])){?>
-					<button type="button" id="siteuser_btn_update" class="btn btn-primary"
-						> Update </button>
-					<button type="submit" name="submit" id="siteuser_update" class="btn btn-primary hidden"
-						value="Update"> Update </button>
-					<button type="button" class="btn btn-primary"
-						onclick="window.location='<?php echo $url; ?>';"> Cancel </button>
-				<?php }else{?>
-					<button type="button" id="siteuser_btn" class="btn btn-primary"
-						value="Submit"> Submit </button>
-					<button type="submit" name="submit" id="siteuser_sbmit"
-						class="btn btn-primary hidden" value="Submit"> Submit </button>
-					<button type="button" class="btn btn-primary"
-						onclick="window.location='<?php echo $url; ?>';"> Cancel </button>
-				<?php }?>
+							echo "<div class='col-sm-4'><div class='checkbox'";
+							if($object->num_rows > 0)
+							{
+								echo "style='background:#ccc;'><input type='checkbox' checked='checked' name='usergame[]' value='{$row['Game_ID']}'>" . $row['Game_Name'];
+							}
+							else{
+								echo "><input type='checkbox' name='usergame[]' value='{$row['Game_ID']}'>" . $row['Game_Name'];
+							}
+							echo "</div></div>";
+						}
+						?>
+					</div>
 				</div>
 			</div>
-		</div>
-	</form>
-</div>
-<div class="clearfix"></div>
+			<div class="row">
+				<div class="col-sm-12">
+					<div class="form-group text-center">
+						<?php if(isset($_GET['edit']) && !empty($_GET['edit'])){?>
+							<button type="button" id="siteuser_btn_update" class="btn btn-primary"
+							> Update </button>
+							<button type="submit" name="submit" id="siteuser_update" class="btn btn-primary hidden"
+							value="Update"> Update </button>
+							<button type="button" class="btn btn-primary"
+							onclick="window.location='<?php echo $url; ?>';"> Cancel </button>
+						<?php }else{?>
+							<button type="button" id="siteuser_btn" class="btn btn-primary"
+							value="Submit"> Submit </button>
+							<button type="submit" name="submit" id="siteuser_sbmit"
+							class="btn btn-primary hidden" value="Submit"> Submit </button>
+							<button type="button" class="btn btn-primary"
+							onclick="window.location='<?php echo $url; ?>';"> Cancel </button>
+						<?php }?>
+					</div>
+				</div>
+			</div>
+		</form>
+	</div>
+	<div class="clearfix"></div>
 
-<script>
-<!--
+	<script>
+		<!--
 
 //$("#siteuser_frm").validate({
 //	rules: {
@@ -128,16 +133,24 @@ span.alert-danger {
 //	}
 //});
 
-$('#siteuser_btn').click( function(){
-//	if($("#siteuser_frm").valid()){		
-		$( "#siteuser_sbmit" ).trigger( "click" );
-//	}
-});
+$(document).ready(function(){
+	// selecte all
+	$('#select_all').on('click',function(){
+		if($(this).is(':checked')){
+			$('input[type="checkbox"]').prop('checked',true);
+		}
+		else
+		{
+			$('input[type="checkbox"]').prop('checked',false);
+		}
+	});
 
-$('#siteuser_btn_update').click( function(){
-//	if($("#siteuser_frm").valid()){
+	$('#siteuser_btn').click( function(){
+		$( "#siteuser_sbmit" ).trigger( "click" );
+	});
+	$('#siteuser_btn_update').click( function(){
 		$( "#siteuser_update" ).trigger( "click" );
-//	}
+	});
 });
 
 // -->
