6aeefba6a25486ef11ee89045f9df69c6ef51cb2
corporatesim <das_debasish@hotmail.com>
2018-11-01 14:20:02 +0530

    * overlay div color changed to light black * chart file indented

--- chart/multidepthpie.php
+++ chart/multidepthpie.php
@@ -7,75 +7,67 @@ $functionsObj = new Functions();
 
 include_once '../includes/lib/phpchartdir.php';
 
+$ChartID            = $_GET['ChartID'];
+$gameid             = $_GET['gameid'];
+$userid             = $_GET['userid'];
 
-$ChartID = $_GET['ChartID'];
-$gameid  = $_GET['gameid'];
-$userid = $_GET['userid'];
+$sqlchart           = "SELECT * FROM GAME_CHART WHERE Chart_Status=1 and Chart_ID =".$ChartID;
+$chartDetails       = $functionsObj->ExecuteQuery($sqlchart);
+$ResultchartDetails = $functionsObj->FetchObject($chartDetails);
 
-	$sqlchart = "SELECT * FROM GAME_CHART WHERE Chart_Status=1 and Chart_ID =".$ChartID;
-	$chartDetails = $functionsObj->ExecuteQuery($sqlchart);
-	$ResultchartDetails= $functionsObj->FetchObject($chartDetails);
-
-
-	$sqllink = "SELECT * FROM `GAME_LINKAGE` WHERE `Link_GameID`=".$gameid." AND Link_ScenarioID= ".$ResultchartDetails->Chart_ScenarioID;
-	$LinkId =  $functionsObj->ExecuteQuery($sqllink);
-	$ResultLinkId = $functionsObj->FetchObject($LinkId);
+$sqllink            = "SELECT * FROM `GAME_LINKAGE` WHERE `Link_GameID`=".$gameid." AND Link_ScenarioID= ".$ResultchartDetails->Chart_ScenarioID;
+$LinkId             =  $functionsObj->ExecuteQuery($sqllink);
+$ResultLinkId       = $functionsObj->FetchObject($LinkId);
 
 
-	$sqlexist="SELECT * FROM `GAME_INPUT` i WHERE i.input_user=".$userid." and i.input_sublinkid in 
-	(SELECT s.SubLink_ID FROM GAME_LINKAGE_SUB s WHERE s.SubLink_LinkID=".$ResultLinkId->Link_ID.") group by i.input_sublinkid";
-	$object = $functionsObj->ExecuteQuery($sqlexist);
-
+$sqlexist = "SELECT * FROM `GAME_INPUT` i WHERE i.input_user=".$userid." and i.input_sublinkid in 
+(SELECT s.SubLink_ID FROM GAME_LINKAGE_SUB s WHERE s.SubLink_LinkID=".$ResultLinkId->Link_ID.") group by i.input_sublinkid";
+$object = $functionsObj->ExecuteQuery($sqlexist);
 
 	//chart_component and subcomponent
 
-	$comp = $ResultchartDetails->Chart_Components;
-	$subcomp = $ResultchartDetails->Chart_Subcomponents;
-	$chartname = $ResultchartDetails->Chart_Name;
-	$charttype = $ResultchartDetails->Chart_Type;
-	
+$comp      = $ResultchartDetails->Chart_Components;
+$subcomp   = $ResultchartDetails->Chart_Subcomponents;
+$chartname = $ResultchartDetails->Chart_Name;
+$charttype = $ResultchartDetails->Chart_Type;
 
-	$arrayComp = explode(',',$comp);
-	$arraysubcomp = explode(',',$subcomp);
+
+$arrayComp    = explode(',',$comp);
+$arraysubcomp = explode(',',$subcomp);
 	//print_r($arrayComp);
 
 	//echo $object->num_rows;
-	if($object->num_rows > 0){
-		
-		while($row= $object->fetch_object()){
-			
-			$compdata = $row->input_key;
-			$allcompID = explode('_',$compdata);
-			$compID = $allcompID[2];
-			
-			if (in_array($compID, $arrayComp) && $allcompID[1]=='comp')
-			{
-				$sqlcompName = "SELECT Comp_Name FROM GAME_COMPONENT WHERE Comp_ID = ".$compID;
-				$compDetails = $functionsObj->ExecuteQuery($sqlcompName);
-				$compName= $functionsObj->FetchObject($compDetails);
-				
-				$dataChart[$compName->Comp_Name]  = $row->input_current;
-			}
-			if (in_array($compID, $arraysubcomp) && $allcompID[1]=='subc')
-			{
-				$sqlsubcompName = "SELECT SubComp_Name FROM GAME_SUBCOMPONENT WHERE SubComp_ID = ".$compID;
-				$subcompDetails = $functionsObj->ExecuteQuery($sqlsubcompName);
-				$subcompName= $functionsObj->FetchObject($subcompDetails);
-				
-				$dataChart[$subcompName->SubComp_Name]  .= $row->input_current;
-			}
-			
-			
+if($object->num_rows > 0){
+
+	while($row = $object->fetch_object()){
+
+		$compdata  = $row->input_key;
+		$allcompID = explode('_',$compdata);
+		$compID    = $allcompID[2];
+
+		if (in_array($compID, $arrayComp) && $allcompID[1]=='comp')
+		{
+			$sqlcompName                     = "SELECT Comp_Name FROM GAME_COMPONENT WHERE Comp_ID = ".$compID;
+			$compDetails                     = $functionsObj->ExecuteQuery($sqlcompName);
+			$compName                        = $functionsObj->FetchObject($compDetails);
+			$dataChart[$compName->Comp_Name] = $row->input_current;
+		}
+		if (in_array($compID, $arraysubcomp) && $allcompID[1]=='subc')
+		{
+			$sqlsubcompName                         = "SELECT SubComp_Name FROM GAME_SUBCOMPONENT WHERE SubComp_ID = ".$compID;
+			$subcompDetails                         = $functionsObj->ExecuteQuery($sqlsubcompName);
+			$subcompName                            = $functionsObj->FetchObject($subcompDetails);
+			$dataChart[$subcompName->SubComp_Name] .= $row->input_current;
 		}
 	}
+}
 
+foreach($dataChart as $key=>$val)
+{
+	$labels [] = $key;
+	$data []   = $val;
 
-	foreach($dataChart as $key=>$val)
-	{
-		$labels []= $key;
-		$data []= $val;
-		
-	}
+}
 
 # The data for the pie chart
 //$data = array(72, 18, 15, 12);
@@ -85,10 +77,9 @@ $userid = $_GET['userid'];
 
 # The depths for the sectors
 $depths = array(30, 20, 10, 10);
-
 # Create a PieChart object of size 360 x 300 pixels, with a light blue (DDDDFF) background and a 1
 # pixel 3D border
-$c = new PieChart(800, 300, 0xddddff, -1, 1);
+$c      = new PieChart(800, 300, 0xddddff, -1, 1);
 
 # Set the center of the pie at (180, 175) and the radius to 100 pixels
 $c->setPieSize(380, 150, 120);
