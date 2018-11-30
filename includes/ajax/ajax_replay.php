1a9b6c1234632dd5ebadb396049226853772ee97
corporatesim <das_debasish@hotmail.com>
2018-11-30 12:57:27 +0530

    * ajaxReplay file added

---
+++ includes/ajax/ajax_replay.php
@@ -0,0 +1,53 @@
+<?php
+
+include_once '../../config/settings.php';
+include_once '../../config/functions.php';
+set_time_limit(300);
+
+$funObj    = new Functions();
+
+$UserID    = $_SESSION['userid'];
+
+/*print_r($userid);
+print_r($gameid);*/
+
+if($_POST['action'] == 'replay')
+{
+	$GameID    = $_POST['GameID'];
+	$ScenID    = $_POST['ScenID'];
+	$LinkID    = $_POST['LinkID'];
+
+	$updateSql = "UPDATE GAME_USERSTATUS SET US_LinkID = 0, US_Output = 0 WHERE US_UserID = $UserID AND US_GameID = $GameID AND US_ScenID = $ScenID ";
+	// echo $sql.'<br>';
+	$resultUpdate = $funObj->ExecuteQuery($updateSql);
+
+	$deleteSql = " DELETE FROM GAME_INPUT WHERE input_user = $UserID AND input_sublinkid IN( SELECT SubLink_Id FROM GAME_LINKAGE_SUB WHERE SubLink_LinkID = ( SELECT Link_ID FROM GAME_LINKAGE WHERE Link_GameID = $GameID AND Link_ScenarioID = $ScenID ))";
+	// echo $deleteSql;
+	$resultDelete = $funObj->ExecuteQuery($deleteSql);
+
+	$updTimer = " UPDATE  GAME_LINKAGE_TIMER  gt  SET  gt.timer = (
+	SELECT SUM( (gl.Link_Hour * 60) + gl.Link_Min) FROM GAME_LINKAGE gl
+	WHERE gl.Link_GameID = $GameID  AND gl.Link_ScenarioID = $ScenID) WHERE
+	gt.linkid = $LinkID  AND gt.userid = $UserID ";
+	$funObj->ExecuteQuery($updTimer);
+  /*print_r($updTimer);
+  die();*/
+	// print_r($resultUpdate);	print_r($resultDelete);	die('here');
+	//echo $updateSql.'<br>'.$deleteSql; die();
+	if($resultUpdate && $resultDelete)
+	{
+		echo 'redirect';
+	}
+	elseif($resultUpdate)
+	{
+		echo 'User Status Update';
+	}
+	elseif($resultDelete)
+	{
+		echo 'User Record Deleted';
+	}
+	else
+	{
+		echo 'Database Connection Error';
+	}
+}
