eb573106974f7c67e54c82d907e92255c17975ba
corporatesim <das_debasish@hotmail.com>
2019-01-08 12:21:37 +0530

    * areaName and component name added as title on formula pages

--- ux-admin/controller/Formulas.php
+++ ux-admin/controller/Formulas.php
@@ -1,50 +1,59 @@
 <?php 
 $funObj = new Model();
-	$f_list = $funObj->SelectData(array(), formulas, array(), '', '', '', '', 0);
-	$file = 'list.php';
+$f_list = $funObj->SelectData(array(), formulas, array(), '', '', '', '', 0);
+$file   = 'list.php';
 
-if( isset( $_GET['add'] ) && !empty( $_GET['add'] ) ){
-	$head = "Add Formula";
-	$components = $funObj->SelectData(array(), component, array(), 'Comp_Name', '', '', '', 0);
-	$subcomponents = $funObj->SelectData(array(), subcomponent, array(), 'SubComp_Name', '', '', '', 0);
-	$operators = $funObj->SelectData(array(), math_operators, array(), '', '', '', '', 0);
+if( isset( $_GET['add'] ) && !empty( $_GET['add'] ) )
+{
+	$head               = "Add Formula";
+	$componentsQuery    = 'SELECT gc.*,ga.Area_Name AS AreaName FROM GAME_COMPONENT gc LEFT JOIN GAME_AREA ga ON ga.Area_ID= gc.Comp_AreaID WHERE gc.Comp_Delete=0 ORDER BY gc.Comp_Name';
+	$subcomponentsQuery = 'SELECT gs.*,gc.Comp_Name AS ComponentName,ga.Area_Name AS AreaName FROM GAME_SUBCOMPONENT gs LEFT JOIN GAME_AREA ga ON ga.Area_ID = gs.SubComp_AreaID LEFT JOIN GAME_COMPONENT gc ON gc.Comp_ID=gs.SubComp_CompID WHERE gs.SubComp_Delete=0 ORDER BY gs.SubComp_Name';
+	$areaComponents     = $funObj->ExecuteQuery($componentsQuery);
+	$areaSubcomponents  = $funObj->ExecuteQuery($subcomponentsQuery);
+	$components         = $funObj->SelectData(array(), component, array(), 'Comp_Name', '', '', '', 0);
+	$subcomponents      = $funObj->SelectData(array(), subcomponent, array(), 'SubComp_Name', '', '', '', 0);
+	$operators          = $funObj->SelectData(array(), math_operators, array(), '', '', '', '', 0);
 	
 	$file = 'add_edit_formula.php';
-}elseif( isset( $_GET['edit'] ) && !empty( $_GET['edit'] ) ){
+}
+elseif( isset( $_GET['edit'] ) && !empty( $_GET['edit'] ) )
+{
 	$head = "Add Formula";
-	$fid =base64_decode($_GET['edit']);
+	$fid  = base64_decode($_GET['edit']);
 	
-	$components = $funObj->SelectData(array(), component, array(), 'Comp_Name', '', '', '', 0);
+	$components    = $funObj->SelectData(array(), component, array(), 'Comp_Name', '', '', '', 0);
 	$subcomponents = $funObj->SelectData(array(), subcomponent, array(), 'SubComp_Name', '', '', '', 0);
-	$operators = $funObj->SelectData(array(), math_operators, array(), '', '', '', '', 0);
-	
-	$formula = $funObj->SelectData(array(), formulas, array('f_id='.$fid), '', '', '', '', 0);
-	$details=$functionsObj->FetchObject($formula);
+	$operators     = $funObj->SelectData(array(), math_operators, array(), '', '', '', '', 0);
+	$formula       = $funObj->SelectData(array(), formulas, array('f_id='.$fid), '', '', '', '', 0);
+	$details       = $functionsObj->FetchObject($formula);
 	//exit();
-	$file = 'add_edit_formula.php';
-}elseif( isset( $_GET['del'] ) && !empty( $_GET['del'] ) ){
-	$fid =base64_decode($_GET['del']);
+	$file          = 'add_edit_formula.php';
+}
+elseif( isset( $_GET['del'] ) && !empty( $_GET['del'] ) )
+{
+	$fid    = base64_decode($_GET['del']);
 	
 	$object = $functionsObj->SelectData(array(), 'GAME_LINKAGE_SUB', array('SubLink_FormulaID='.$fid), '', '', '', '', 0);
 	if ($object->num_rows > 0) {
-		$msg = 'Cannot Delete Formula! It is already in use';
+		$msg      = 'Cannot Delete Formula! It is already in use';
 		$type [0] = 'inputError';
 		$type [1] = 'has-error';		
 	}
-	else{
-		$result = $functionsObj->DeleteData('GAME_FORMULAS','f_id',$fid,0);
-
-		$_SESSION ['msg'] = 'Formula Deleted';
+	else
+	{
+		$result               = $functionsObj->DeleteData('GAME_FORMULAS','f_id',$fid,0);
+		$_SESSION ['msg']     = 'Formula Deleted';
 		$_SESSION ['type[0]'] = 'inputSuccess';
 		$_SESSION ['type[1]'] = 'has-success';
 		header ( "Location:" . site_root . "ux-admin/Formulas" );
 		exit(0);	
 	}
 
-} else {
+} 
+else
+{
 	$f_list = $funObj->SelectData(array(), formulas, array(), '', '', '', '', 0);
-	$file = 'list.php';
+	$file   = 'list.php';
 }
 
 include_once doc_root.'ux-admin/view/formula/'.$file;
-?>
--- ux-admin/view/formula/add_edit_formula.php
+++ ux-admin/view/formula/add_edit_formula.php
@@ -1,7 +1,8 @@
+<!-- <?php // echo "<pre>"; print_r($subcomponents->fetch_object()); exit; ?> -->
 <script type="text/javascript">
-<!--
-var loc_url_del = "ux-admin/ManageGame/del/";
-var loc_url_stat = "ux-admin/ManageGame/stat/";
+	<!--
+		var loc_url_del = "ux-admin/ManageGame/del/";
+		var loc_url_stat = "ux-admin/ManageGame/stat/";
 //-->
 </script>
 
@@ -56,157 +57,155 @@ var loc_url_stat = "ux-admin/ManageGame/stat/";
 		<ul class="breadcrumb">
 			<li class="completed"><a
 				href="<?php echo site_root."ux-admin/Dashboard"; ?>">Home</a></li>
-			<li class="completed">
-				<a href="javascript:void(0);"> Master Management</a></li>
-			<li class="active"><a href="<?php echo site_root; ?>ux-admin/Formulas"> Formulas</a></li>
-			<li class="active"><?php echo $head; ?></li>
-		</ul>
-	</div>
-</div>
-
-<div class="error_box">
-	<label></label>
-</div>
-
-<div class="row">
-	<div class="col-sm-12">
-		<div class="row">
-			<div class="formula_box">			
-				<div class="col-xs-12 col-sm-12 col-md-9 form-group">
-					<label>Formula title</label>
-					<input type="hidden" name="id" id="id" 
-					value="<?php if(isset($_GET['edit'])){ echo $details->f_id; } ?>">
-					<input type="text" name="formula_title" 
-					value="<?php if(!empty($details->formula_title)) echo $details->formula_title; ?>"
-					id="formula_title" class="form-control" />
-				</div>
-			</div>
-			<?php if(!empty($details->expression_string)) {?>
-			<div class="formula_box">
-				<div class="col-xs-12 col-sm-8 col-md-8 form-group">
-					<label>View Original Formula</label>
-					<textarea name="formula_view" id="formula_view" class="form-control" rows="5" readonly><?php if(!empty($details->expression_string)) echo $details->expression_string; ?></textarea>
-				</div>
-			</div>
-			<?php } ?>
-			<div class="formula_box">	
-				<div class="col-sm-6">
-				<label>Count of ( : </label><label id="openbracket" name="openbracket" >0</label>
-				</div>
-				<div class="col-sm-6">
-				<label>Count of ) : </label><label id="closebracket" name="closebracket" >0</label>
-				</div>
-			</div>
-			<div class="formula_box">				
-				<div class="col-xs-12 col-sm-8 col-md-8 form-group">
-					<label>Create Formula</label>
-					<textarea name="formula" id="formula" class="form-control" rows="10" readonly><?php if(!empty($details->expression_string)) echo $details->expression_string; ?></textarea>
-				</div>
-				<div class="col-xs-12 col-sm-4 col-md-4">
-					<button type="button" class="btn btn-default undo_btn" style="">Undo</button>
-				</div>
-			</div>
-			<div class="clearfix"></div>
-			
-			<!-- Games -->
-			<div class="col-xs-12 col-sm-4 col-md-4 form-group">
-				<label><span class="alert-danger">*</span>Select Game</label> 
-				<select class="form-control" name="game_id" id="game_id">
-					<option value="">-- SELECT --</option>
-						<?php 
-						// Fetch Services list
-						$game = $functionsObj->SelectData(array(), 'GAME_GAME', array('Game_Delete=0'), '', '', '', '', 0);
-
-						while($row = $game->fetch_object()){ ?>
-							<option value="<?php echo $row->Game_ID; ?>">
-								<?php echo $row->Game_Name; ?>
-							</option>
-						<?php } ?>
-				</select>
-			</div>
-			<!-- END Games -->
-
-			<!-- Scenario -->
-			<div class="col-xs-12 col-sm-4 col-md-4 form-group">
-				<label><span class="alert-danger">*</span>Select Scenario</label> 
-				<select class="form-control" name="scen_id" id="scen_id">
-					<option value="">-- SELECT --</option>
-						<?php 
-						// Fetch Services list
-						$scenario = $functionsObj->SelectData(array(), 'GAME_SCENARIO', array('Scen_Delete=0'), '', '', '', '', 0);
-
-						while($row = $scenario->fetch_object()){ ?>
-							<option value="<?php echo $row->Link_ID; ?>">
-								<?php echo $row->Scen_Name; ?>
-							</option>
-						<?php } ?>
-				</select>
-			</div>
-			<!-- END Games -->
-			
-			<div class="clearfix"></div>	
-			
-			<!-- Components -->
-			<div class="col-xs-12 col-sm-4 col-md-4 form-group">
-				<label>Components </label>
-				<ul class="components" name="comp_id" id="comp_id">
-				<?php
-					if( $components->num_rows > 0 ) {
-						while( $rows = $components->fetch_object() ){ ?>
-							<li id="comp_<?php echo $rows->Comp_ID; ?>" value="{<?php echo $rows->Comp_Name; ?>}">
-								<?php echo $rows->Comp_Name; ?>
-							</li><?php
-						}
-					} ?>
+				<li class="completed">
+					<a href="javascript:void(0);"> Master Management</a></li>
+					<li class="active"><a href="<?php echo site_root; ?>ux-admin/Formulas"> Formulas</a></li>
+					<li class="active"><?php echo $head; ?></li>
 				</ul>
-			</div>
-			<!-- END Components -->
-			
-			<!-- Sub Components -->
-			<div class="col-xs-12 col-sm-4 col-md-4 form-group">
-				<label>Sub Components </label>
-				<ul class="subcomponents" name="subcomp_id" id="subcomp_id">
-				<?php
-					if( $subcomponents->num_rows > 0 ) {
-						while( $rows = $subcomponents->fetch_object() ){ ?>
-							<li id="subc_<?php echo $rows->SubComp_ID; ?>" value="{<?php echo $rows->SubComp_Name; ?>}">
-								<?php echo $rows->SubComp_Name; ?>
-							</li><?php
-						}
-					} ?>
-				</ul>
-			</div><!-- END Sub Components -->
-			
-			<!-- Math Operators -->
-			<div class="col-xs-12 col-sm-4 col-md-1 form-group">
-				<label>Operators</label>
-				<ul class="operators"><?php
-					if( $operators->num_rows > 0 ) {
-						while( $rows = $operators->fetch_object() ){ ?>
-							<li id="<?php echo $rows->operator; ?>" value="{<?php echo $rows->operator; ?>}">
-								<?php echo $rows->operator; ?>
-							</li><?php
-						}
-					} ?>
-				</ul>
-			</div><!-- END Math Operators -->
-			
-			<div class="col-xs-12 col-sm-12 col-md-8 form-group text-center">
-				<button type="button" class="btn btn-primary" name="add_formula" id="add_formula">Submit</button>
 			</div>
 		</div>
-    </div>
-</div>
-<div class="clearfix"></div>
 
-<script type="text/javascript">
+		<div class="error_box">
+			<label></label>
+		</div>
+
+		<div class="row">
+			<div class="col-sm-12">
+				<div class="row">
+					<div class="formula_box">			
+						<div class="col-xs-12 col-sm-12 col-md-9 form-group">
+							<label>Formula title</label>
+							<input type="hidden" name="id" id="id" 
+							value="<?php if(isset($_GET['edit'])){ echo $details->f_id; } ?>">
+							<input type="text" name="formula_title" 
+							value="<?php if(!empty($details->formula_title)) echo $details->formula_title; ?>"
+							id="formula_title" class="form-control" />
+						</div>
+					</div>
+					<?php if(!empty($details->expression_string)) {?>
+						<div class="formula_box">
+							<div class="col-xs-12 col-sm-8 col-md-8 form-group">
+								<label>View Original Formula</label>
+								<textarea name="formula_view" id="formula_view" class="form-control" rows="5" readonly><?php if(!empty($details->expression_string)) echo $details->expression_string; ?></textarea>
+							</div>
+						</div>
+					<?php } ?>
+					<div class="formula_box">	
+						<div class="col-sm-6">
+							<label>Count of ( : </label><label id="openbracket" name="openbracket" >0</label>
+						</div>
+						<div class="col-sm-6">
+							<label>Count of ) : </label><label id="closebracket" name="closebracket" >0</label>
+						</div>
+					</div>
+					<div class="formula_box">				
+						<div class="col-xs-12 col-sm-8 col-md-8 form-group">
+							<label>Create Formula</label>
+							<textarea name="formula" id="formula" class="form-control" rows="10" readonly><?php if(!empty($details->expression_string)) echo $details->expression_string; ?></textarea>
+						</div>
+						<div class="col-xs-12 col-sm-4 col-md-4">
+							<button type="button" class="btn btn-default undo_btn" style="">Undo</button>
+						</div>
+					</div>
+					<div class="clearfix"></div>
+
+					<!-- Games -->
+					<div class="col-xs-12 col-sm-4 col-md-4 form-group">
+						<label><span class="alert-danger">*</span>Select Game</label> 
+						<select class="form-control" name="game_id" id="game_id">
+							<option value="">-- SELECT --</option>
+							<?php 
+						// Fetch Services list
+							$game = $functionsObj->SelectData(array(), 'GAME_GAME', array('Game_Delete=0'), '', '', '', '', 0);
+							while($row = $game->fetch_object()){ ?>
+								<option value="<?php echo $row->Game_ID; ?>">
+									<?php echo $row->Game_Name; ?>
+								</option>
+							<?php } ?>
+						</select>
+					</div>
+					<!-- END Games -->
+
+					<!-- Scenario -->
+					<div class="col-xs-12 col-sm-4 col-md-4 form-group">
+						<label><span class="alert-danger">*</span>Select Scenario</label> 
+						<select class="form-control" name="scen_id" id="scen_id">
+							<option value="">-- SELECT --</option>
+							<?php 
+						// Fetch Services list
+							$scenario = $functionsObj->SelectData(array(), 'GAME_SCENARIO', array('Scen_Delete=0'), '', '', '', '', 0);
+							while($row = $scenario->fetch_object()){ ?>
+								<option value="<?php echo $row->Link_ID; ?>">
+									<?php echo $row->Scen_Name; ?>
+								</option>
+							<?php } ?>
+						</select>
+					</div>
+					<!-- END Games -->
+
+					<div class="clearfix"></div>	
+
+					<!-- Components -->
+					<div class="col-xs-12 col-sm-4 col-md-4 form-group">
+						<label>Components </label>
+						<ul class="components" name="comp_id" id="comp_id">
+							<?php
+							if( $components->num_rows > 0 ) {
+								while( $rows = $areaComponents->fetch_object() ){ ?>
+									<li id="comp_<?php echo $rows->Comp_ID; ?>" value="{<?php echo $rows->Comp_Name; ?>}" data-toggle="tooltip" title="<?php echo $rows->AreaName; ?>">
+										<?php echo $rows->Comp_Name; ?>
+										</li><?php
+									}
+								} ?>
+							</ul>
+						</div>
+						<!-- END Components -->
+
+						<!-- Sub Components -->
+						<div class="col-xs-12 col-sm-4 col-md-4 form-group">
+							<label>Sub Components </label>
+							<ul class="subcomponents" name="subcomp_id" id="subcomp_id">
+								<?php
+								if( $subcomponents->num_rows > 0 ) {
+									while( $rows = $areaSubcomponents->fetch_object() ){ ?>
+										<li id="subc_<?php echo $rows->SubComp_ID; ?>" value="{<?php echo $rows->SubComp_Name; ?>}" data-toggle="tooltip" title="<?php echo 'Area: '.$rows->AreaName.', Component: '.$rows->ComponentName;?>">
+											<?php echo $rows->SubComp_Name; ?>
+											</li><?php
+										}
+									} ?>
+								</ul>
+							</div><!-- END Sub Components -->
+
+							<!-- Math Operators -->
+							<div class="col-xs-12 col-sm-4 col-md-1 form-group">
+								<label>Operators</label>
+								<ul class="operators"><?php
+								if( $operators->num_rows > 0 ) {
+									while( $rows = $operators->fetch_object() ){ ?>
+										<li id="<?php echo $rows->operator; ?>" value="{<?php echo $rows->operator; ?>}">
+											<?php echo $rows->operator; ?>
+											</li><?php
+										}
+									} ?>
+								</ul>
+							</div><!-- END Math Operators -->
+
+							<div class="col-xs-12 col-sm-12 col-md-8 form-group text-center">
+								<button type="button" class="btn btn-primary" name="add_formula" id="add_formula">Submit</button>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class="clearfix"></div>
+
+				<script type="text/javascript">
 // <!--
-	var formula = [];
-	var formula_string = [];
-	
+var formula = [];
+var formula_string = [];
+
 	//$('.components li, .subcomponents li, .operators li').click( function(){
-	$('.components, .subcomponents, .operators').on('click', 'li', function () {
-		formula.push( $( this ).attr( 'id' ) );
+		$('.components, .subcomponents, .operators').on('click', 'li', function () {
+			formula.push( $( this ).attr( 'id' ) );
 		// console.log( formula.join(" ") );
 		//alert($( this ).attr('value'));
 		if($( this ).attr('value') == '{(}'){
@@ -219,9 +218,9 @@ var loc_url_stat = "ux-admin/ManageGame/stat/";
 		$('#formula').text( formula_string.join(" ") );
 	});
 
-	$('.undo_btn').click( function(){
-		var fpop = formula.pop();
-		var fspop = formula_string.pop();
+		$('.undo_btn').click( function(){
+			var fpop = formula.pop();
+			var fspop = formula_string.pop();
 		//alert(fpop);
 		//alert(fspop);
 		if(fspop == '{(}'){
@@ -233,15 +232,15 @@ var loc_url_stat = "ux-admin/ManageGame/stat/";
 		$('#formula').text( formula_string.join(" ") );
 	});
 
-	$('#add_formula').click( function(){
-		if( $('#formula').val() != '' ){
-			
-			$('.error_box label').removeClass('error');
-			$('.error_box label').html('');
-			var formula_title = $('#formula_title').val();
-			var formula_exp = formula.join(" ");
-			var formula_str = formula_string.join(" ");
-			var formula_id = $('#id').val();
+		$('#add_formula').click( function(){
+			if( $('#formula').val() != '' ){
+
+				$('.error_box label').removeClass('error');
+				$('.error_box label').html('');
+				var formula_title = $('#formula_title').val();
+				var formula_exp = formula.join(" ");
+				var formula_str = formula_string.join(" ");
+				var formula_id = $('#id').val();
 			//alert(formula_id); 
 			//alert("formula_exp "+formula_exp);
 			//alert("formula_str "+formula_str);
@@ -278,9 +277,9 @@ var loc_url_stat = "ux-admin/ManageGame/stat/";
 			}, 'fast');
 		}
 	});
-	
-	$('#game_id').change( function(){
-		var game_id = $(this).val();
+
+		$('#game_id').change( function(){
+			var game_id = $(this).val();
 		//alert(comp_id);
 		$('#scen_id').html('<option value="">-- SELECT --</option>');
 
@@ -293,9 +292,9 @@ var loc_url_stat = "ux-admin/ManageGame/stat/";
 			}
 		});		
 	});
-	
-	$('#scen_id').change( function(){
-		var scen_id = $(this).val();
+
+		$('#scen_id').change( function(){
+			var scen_id = $(this).val();
 		//alert(scen_id);		
 
 		$.ajax({
@@ -316,6 +315,6 @@ var loc_url_stat = "ux-admin/ManageGame/stat/";
 			}
 		});		
 	});
-	
+
 //-->
 </script>
