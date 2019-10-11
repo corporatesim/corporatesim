84ae9646bee1cd815e6d280d6db827a1fb3ade72
Mohit Kumar Sahu <mksahu23506@gmail.com>
2019-10-11 12:37:38 +0530

    * chart image aligned to center

--- chart/threeddonutshading.php
+++ chart/threeddonutshading.php
@@ -73,12 +73,12 @@ $chartIndex = (int)($_REQUEST["img"]);
 $colors = array(0xFF6633, 0xFFB399, 0xFF33FF, 0xFFFF99, 0x00B3E6, 0xE6B333, 0x3366E6, 0x999966, 0x99FF99, 0xB34D4D, 0x80B300, 0x809900, 0xE6B3B3, 0x6680B3, 0x66991A,  0xFF99E6, 0xCCFF1A, 0xFF1A66, 0xE6331A, 0x33FFCC, 0x66994D, 0xB366CC, 0x4D8000, 0xB33300, 0xCC80CC,  0x66664D, 0x991AFF, 0xE666FF, 0x4DB3FF, 0x1AB399, 0xE666B3, 0x33991A, 0xCC9999, 0xB3B31A, 0x00E680,  0x4D8066, 0x809980, 0xE6FF80, 0x1AFF33, 0x999933, 0xFF3380, 0xCCCC00, 0x66E64D, 0x4D80CC, 0x9900B3,  0xE64D66, 0x4DB380, 0xFF4D4D, 0x99E6E6, 0x6666FF);
 # Create a PieChart object of size 200 x 200 pixels. Use a vertical gradient color from blue
 # (0000cc) to deep blue (000044) as background. Use rounded corners of 16 pixels radius.
-$c = new PieChart(200, 200);
+$c = new PieChart(400, 400);
 $c->setBackground($c->linearGradientColor(0, 0, 0, $c->getHeight(), 0x0000cc, 0x000044));
 $c->setRoundedFrame(0xffffff, 16);
 
 # Set donut center at (100, 100), and outer/inner radii as 80/40 pixels
-$c->setDonutSize(100, 100, 80, 40);
+$c->setDonutSize(200, 190, 110, 60);
 
 # Set the pie data
 $c->setData($data);
@@ -91,7 +91,8 @@ $c->set3D(20);
 
 # Demonstrates various shading modes
 if ($chartIndex == 0) {
-    $c->addTitle("Default Shading", "bold", 12, 0xffffff);
+    // $c->addTitle("Default Shading", "bold", 12, 0xffffff);
+    $c->addTitle("", "bold", 12, 0xffffff);
 } else if ($chartIndex == 1) {
     $c->addTitle("Flat Gradient", "bold", 12, 0xffffff);
     $c->setSectorStyle(FlatShading);
@@ -116,7 +117,7 @@ if ($chartIndex == 0) {
 }
 
 # Disable the sector labels by setting the color to Transparent
-$c->setLabelStyle("", 8, 0xeebb22);
+$c->setLabelStyle("", 8, 0xffffff);
 
 # Output the chart
 header("Content-type: image/png");
